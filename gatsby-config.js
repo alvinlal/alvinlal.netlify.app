1df8a1727217a8b1ab6e86003fbaffc769db7dc9
alvinlal <alvinzzz2001@gmail.com>
2020-06-17 23:08:52 +0530

    added gatsby-plugin-mdx and sharp library

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,41 @@
 module.exports = {
   plugins: [
     {
-      resolve: "gatsby-plugin-filesystem",
+      resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
         path: `${__dirname}/src/pages`,
       },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
       options: {
         name: "posts",
         path: `${__dirname}/src/posts`,
       },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extension: [".md", ".mdx"],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
   ],
 }
