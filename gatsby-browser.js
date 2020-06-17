8f77d26f6b69941637527e65d38ca4b2f17191c2
alvinlal <alvinzzz2001@gmail.com>
2020-06-18 00:38:52 +0530

    added global Styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,26 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 import Theme from "./src/themes/theme"
 
+const GlobalStyles = createGlobalStyle`
+
+   *{
+       box-sizing:border-box;
+       margin:0;
+       padding:0;
+   }
+
+  html, body{
+      font-family:${props => props.theme.fonts.main};
+      height:100%;
+      background-color:${props => props.theme.colors.light};
+  }
+
+`
+
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={Theme}>{element}</ThemeProvider>
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
 )
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,22 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 import Theme from "./src/themes/theme"
+
+const GlobalStyles = createGlobalStyle`
+
+   *{
+       box-sizing:border-box;
+       margin:0;
+       padding:0;
+   }
+
+  html, body{
+      font-family:${props => props.theme.font.main};
+      height:100%;
+      background-color:${props => props.theme.colors.light};
+  }
+
+`
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={Theme}>{element}</ThemeProvider>
