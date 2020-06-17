dbedfa68d16a8cd691b30280862a424bff2ae510
alvinlal <alvinzzz2001@gmail.com>
2020-06-17 22:51:40 +0530

    added gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages`,
+      },
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts`,
+      },
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
